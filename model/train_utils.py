504513d8c38f58388096e71b3d68275947dbcc9d
sangjaekang <sangjaekang48@gmail.com>
2018-06-29 21:57:21 +0900

    gan 학습 관련 util 추가

--- model/train_utils.py
+++ model/train_utils.py
@@ -27,14 +27,14 @@ class PlotCheckpoint(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs={}):
         plot_path = os.path.join(self.plot_dir,"{:02d}_epoch_sample.png".format(epoch))
-        
+
         pred = self.unet.predict_on_batch(self.images)
         if self.bg_removal:
             plot_bg_removal_sample_image(self.images, pred, plot_path)
         else:
             pred = (pred >= self.thr).astype(np.uint8)
             plot_sample_image(self.images, pred, plot_path)
-            
+
         return
 
     def on_batch_begin(self, batch, logs={}):
@@ -130,3 +130,41 @@ def mean_iou(y_pred, y_true):
     intersect = y_pred * y_true
     union = K.ones_like(y_pred) - ((1-y_pred)*(1-y_true))
     return K.sum(intersect) / K.sum(union)
+
+def np_mean_iou(y_pred, y_true, thr=0.5):
+    y_pred = (y_pred>thr).astype(np.int)
+
+    intersect = y_pred * y_true
+    union = np.ones_like(y_pred) - ((1-y_pred) * (1-y_true))
+    return np.sum(intersect) / np.sum(union)
+
+def get_disc_batch(image_batch, profile_batch, generator, batch_counter,
+                   label_smoothing=False, label_flipping=0):
+    batch_size = image_batch.shape[0]
+    # Create X_disc: alternatively only generated or real images
+    if batch_counter % 2 == 0:
+        # Produce an output
+        X_disc = generator.predict(image_batch)
+        y_disc = np.zeros((batch_size, 2), dtype=np.uint8)
+        y_disc[:, 0] = 1
+
+        if label_flipping > 0:
+            p = np.random.binomial(1, label_flipping)
+            if p > 0:
+                y_disc[:, [0, 1]] = y_disc[:, [1, 0]]
+    else:
+        X_disc = profile_batch
+        y_disc = np.zeros((batch_size, 2), dtype=np.uint8)
+        if label_smoothing:
+            y_disc[:, 1] = np.random.uniform(low=0.9, high=1, size=y_disc.shape[0])
+        else:
+            y_disc[:, 1] = 1
+
+        if label_flipping > 0:
+            p = np.random.binomial(1, label_flipping)
+            if p > 0:
+                y_disc[:, [0, 1]] = y_disc[:, [1, 0]]
+
+    X_disc = np.concatenate([image_batch, X_disc],axis=-1)
+
+    return X_disc, y_disc
